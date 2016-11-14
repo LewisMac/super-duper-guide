8ca31ca34897e2c00dc8bc91e704ea9312794b36
Lewis MacNee <lewis.macnee@hotmail.com>
2016-11-14 16:31:09 +0000

    First commit, no optional done

---
+++ crud_homework/marveldb.sql
@@ -0,0 +1,21 @@
+
+-- SELECT * FROM movies;
+
+-- SELECT name FROM people
+-- WHERE id = 14;
+
+-- DELETE FROM movies
+-- WHERE title = 'Batman Begins'
+
+-- INSERT INTO people (name) VALUES ('Val')
+
+-- DELETE FROM people
+-- WHERE id = 11
+
+-- UPDATE people SET name = 'Jeff 3'
+-- WHERE id = 15
+
+-- INSERT INTO movies (title, year, show_time) VALUES ('Guardians of the Galaxy 2', 2016, '00:00');
+
+-- UPDATE movies SET show_time = '21:30'
+-- WHERE title = 'Guardians of the Galaxy'
